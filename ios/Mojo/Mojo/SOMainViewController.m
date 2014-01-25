15d414056e96a48b38d993bffc289a21ff1497f0
James Eggers <james@jameseggers.com>
2014-01-25 16:20:46 +0000

    added parse

--- ios/Mojo/Mojo/SOMainViewController.m
+++ ios/Mojo/Mojo/SOMainViewController.m
@@ -8,6 +8,7 @@
 
 #import "SOMainViewController.h"
 #import <Parse/Parse.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface SOMainViewController ()
 
@@ -19,21 +20,38 @@
 {
     [super viewDidLoad];
     
-    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 300.0)];
-    topView.backgroundColor = [UIColor colorWithRed:1 green:0.212 blue:0.212 alpha:1.0];
+    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 320.0, 480.0)];
+    topView.backgroundColor = [UIColor colorWithRed:0 green:0.624 blue:0.89 alpha:1.0];
     [self.view addSubview:topView];
     
-    UILabel *moodSelectorLabel = [[UILabel alloc] initWithFrame:CGRectMake(55.0, 300.0, 280.0, 50.0)];
+    UILabel *moodSelectorLabel = [[UILabel alloc] initWithFrame:CGRectMake(55.0, 380.0, 280.0, 50.0)];
     moodSelectorLabel.text = @"How are you feeling today?";
-    moodSelectorLabel.textColor = [UIColor colorWithRed:1 green:0.212 blue:0.212 alpha:1.0];
+    moodSelectorLabel.textColor = [UIColor whiteColor];
     [self.view addSubview:moodSelectorLabel];
     
-    UISlider *moodSelectorSlider = [[UISlider alloc] initWithFrame:CGRectMake(55.0, 350.0, 200.0, 60.0)];
-    moodSelectorSlider.tintColor = [UIColor colorWithRed:1 green:0.212 blue:0.212 alpha:1.0];
-    [self.view addSubview:moodSelectorSlider];
+//    UISlider *moodSelectorSlider = [[UISlider alloc] initWithFrame:CGRectMake(55.0, 350.0, 200.0, 60.0)];
+//    moodSelectorSlider.tintColor = [UIColor whiteColor];
+//    [self.view addSubview:moodSelectorSlider];
     
-    [moodSelectorSlider addTarget:self action:@selector(moodChanged:) forControlEvents:UIControlEventTouchUpInside];
+//    [moodSelectorSlider addTarget:self action:@selector(moodChanged:) forControlEvents:UIControlEventTouchUpInside];
     
+    UIView *button = [[UIView alloc] initWithFrame:CGRectMake(35.0, 380.0, 250.0, 50.0)];
+    button.layer.borderColor = [UIColor whiteColor].CGColor;
+    button.layer.cornerRadius = 5;
+    button.layer.borderWidth = 1.0f;
+    button.layer.masksToBounds = YES;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(buttonTapped:)];
+    
+    [self.view addGestureRecognizer:tap];
+    
+    [self.view addSubview:button];
+    
+}
+
+-(void)buttonTapped:(UITapGestureRecognizer *)gr
+{
+    NSLog(@"hi!");
 }
 
 - (IBAction)moodChanged:(UISlider *)sender {
@@ -45,9 +63,12 @@
     [mood saveInBackground];
 }
 
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
 }
+
+
 
 @end
