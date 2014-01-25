835e6876eb69df66a17a28eb063b212e0fcb460a
mattdonnelly <mattdonnelly@me.com>
2014-01-25 11:58:12 +0000

    Added SOPointMeterView and a basic file group structure

---
+++ ios/Mojo/Mojo/SOPointMeterView.m
@@ -0,0 +1,31 @@
+//
+//  SOPointMeterView.m
+//  Mojo
+//
+//  Created by Matt Donnelly on 25/01/2014.
+//  Copyright (c) 2014 SpunOut. All rights reserved.
+//
+
+#import "SOPointMeterView.h"
+
+@implementation SOPointMeterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
