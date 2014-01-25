d9125f64f2f74a0edccd36af33834f19424054c7
James Eggers <james@jameseggers.com>
2014-01-25 12:25:02 +0000

    Mood selector area added, and slider. looks kinda pretty too

--- ios/Mojo/Mojo/SOMainViewController.m
+++ ios/Mojo/Mojo/SOMainViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 300.0, 340.0, 180.0)];
+    footerView.backgroundColor = [UIColor colorWithRed:0.235 green: 0.773 blue:0.0 alpha:1.0];
+    [self.view addSubview:footerView];
+    
+    UILabel *moodSelectorLabel = [[UILabel alloc] initWithFrame:CGRectMake(55.0, 300.0, 280.0, 50.0)];
+    moodSelectorLabel.text = @"How are you feeling today?";
+    moodSelectorLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:moodSelectorLabel];
+    
+    UISlider *moodSelectorSlider = [[UISlider alloc] initWithFrame:CGRectMake(55.0, 350.0, 200.0, 60.0)];
+    [self.view addSubview:moodSelectorSlider];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
