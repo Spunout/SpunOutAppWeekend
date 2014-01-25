022411135e65b0ed2c73786ac150aa8004cc5c9c
mattdonnelly <mattdonnelly@me.com>
2014-01-25 11:44:10 +0000

    Added XCode Project and iOS .gitignore

---
+++ ios/Mojo/Mojo/SOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SOAppDelegate.h
+//  Mojo
+//
+//  Created by Matt Donnelly on 25/01/2014.
+//  Copyright (c) 2014 SpunOut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
