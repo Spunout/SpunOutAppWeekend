91147b9ef4d544d559d4be79267851ef6239d96b
mattdonnelly <mattdonnelly@me.com>
2014-01-25 11:53:14 +0000

    Added Main View Controller

---
+++ ios/Mojo/Mojo/SOMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SOMainViewController.h
+//  Mojo
+//
+//  Created by Matt Donnelly on 25/01/2014.
+//  Copyright (c) 2014 SpunOut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOMainViewController : UIViewController
+
+@end
---
+++ ios/Mojo/Mojo/SOMainViewController.m
@@ -0,0 +1,27 @@
+//
+//  SOMainViewController.m
+//  Mojo
+//
+//  Created by Matt Donnelly on 25/01/2014.
+//  Copyright (c) 2014 SpunOut. All rights reserved.
+//
+
+#import "SOMainViewController.h"
+
+@interface SOMainViewController ()
+
+@end
+
+@implementation SOMainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
