9028691415ff031602f6dccbde0a57c76309008e
mattdonnelly <mattdonnelly@me.com>
2014-02-16 11:32:22 +0000

    Temporarily removed score deduction

--- ios/Mojo/Miyo/SOAppDelegate.m
+++ ios/Mojo/Miyo/SOAppDelegate.m
@@ -11,6 +11,8 @@
 
 #import <CommonCrypto/CommonCrypto.h>
 
+static NSString *const kSOScoreKeyName = @"SOScoreKeyName";
+
 @implementation SOAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -23,88 +25,7 @@
 
     [self.window makeKeyAndVisible];
 
-    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-
-    if (![self resetPointsIfMonday :prefs])
-    {
-        [self deductPointsIfNotLoggedIn :prefs];
-    }
-
     return YES;
-}
-
-- (bool)resetPointsIfMonday:(NSUserDefaults *)prefs
-{
-    NSDate *now = [NSDate date];
-    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-    bool wasScoreReset = false;
-
-    NSDate *lastScoreReset = [prefs objectForKey:@"lastScoreReset"];
-
-    int daysSinceLastReset = [self getNumberOfDaysSince:now :lastScoreReset];
-
-    if (daysSinceLastReset < 7)
-    {
-        int daysAgo = 0;
-        int interval = 0;
-
-        while (daysAgo < daysSinceLastReset)
-        {
-            if (daysAgo != 0)
-            {
-                interval = 86400 * daysAgo;
-            }
-
-            if ([[gregorian components:NSWeekdayCalendarUnit | NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[now dateByAddingTimeInterval:-interval]] weekday] == 2)
-            {
-                [self resetScore:prefs];
-                wasScoreReset = true;
-            }
-
-            daysAgo++;
-        }
-    }
-    else {
-        [self resetScore :prefs];
-        wasScoreReset = true;
-    }
-
-    return wasScoreReset;
-}
-
-- (void)deductPointsIfNotLoggedIn:(NSUserDefaults *)prefs
-{
-    NSInteger currentScore = [prefs integerForKey:@"score"];
-    NSDate *now = [[NSDate alloc] init];
-    NSDate *lastOpen = [prefs objectForKey:@"lastOpen"];
-
-    // check how many days the user has not logged in for
-
-    int daysSinceLastLogin = [self getNumberOfDaysSince:now :lastOpen];
-    NSNumber *daysSinceLastLoginNum = [NSNumber numberWithInt:daysSinceLastLogin];
-    NSLog(@"%d", [daysSinceLastLoginNum integerValue]);
-
-    if (daysSinceLastLogin > 0)
-    {
-        int pointsLost = daysSinceLastLogin * 15;
-        [prefs setInteger:(currentScore - pointsLost) forKey:@"score"];
-    }
-}
-
-- (void)resetScore:(NSUserDefaults *)prefs
-{
-    [prefs setInteger:150 forKey:@"score"];
-    [prefs setObject:[[NSDate alloc] init] forKey:@"lastScoreReset"];
-    NSLog(@"Reseting score to 150 because Monday has passed.");
-}
-
-- (int)getNumberOfDaysSince:(NSDate *)from :(NSDate *)to
-{
-    NSNumber *timeInterval = [[NSNumber alloc] initWithDouble:[from timeIntervalSinceDate:to]];
-
-    int daysSinceLastLogin = [timeInterval intValue] / 86400;
-
-    return daysSinceLastLogin;
 }
 
 @end
